796459ef71741029818bca292e4c522cda99fc1f
iNelan <clpayala@gmail.com>
2022-06-06 18:57:03 +0000

    version1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -15,6 +15,21 @@ const Home = () => {
 		setTaskList(filterList);
 	};
 
+
+	useEffect(() => {
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/iNelan", {
+            method: "POST", // or 'PUT'
+            body: JSON.stringify([]), // data can be string or {object}!
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .catch((error) => console.error("Error:", error))
+            .then((response) => console.log("Success:", response));
+
+
+
 	return (
 		<div>
 			<ImputTask newTask={newTask} />
